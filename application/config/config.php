d6b9fb97bad73d342fcbfe5b8966211f5482f03b
Goker Tanrisever <tanrisevergoker@hotmail.com>
2017-01-09 23:22:14 +0300

    Base url

--- application/config/config.php
+++ application/config/config.php
@@ -23,8 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost:8080/verdi/';
-// $config['base_url'] = $_ENV['BASE_URL'];
+// $config['base_url'] = 'http://localhost:8080/verdi/';
+$config['base_url'] = $_ENV['BASE_URL'];
 
 /*
 |--------------------------------------------------------------------------
